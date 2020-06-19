17f0a20dee6050669dbdf3128850304db5520487
Cavani Nicolas <cavani.nicolas6@gmail.com>
2020-06-19 19:33:49 +0200

    add doc

--- src/hash.h
+++ src/hash.h
@@ -31,22 +31,62 @@ unsigned int hash_string(const char *str);
 
 
 /* -------------------------------------------------------------------------- */
-/* initH                                                                      */
+/* initHashTable                                                              */
 /*       Initialise la table de hachage de taille HASH_MAX                    */
 /*                                                                            */
-/* En entrée: (void)                                                          */
+/* En entrée: hashTable (cell_t **) : Table de hachage, tableau de pointeurs  */
 /*                                                                            */
-/* En sortie: hashTable (cell_t **) : Table de hachage (tableau contenant les */
-/*            pointeurs vers les structures) (initialises a NULL)             */
+/* En sortie: (void)                                                          */
 /* -------------------------------------------------------------------------- */
 void initHashTable(cell_t * hashTable[HASH_MAX]);
 
+
+/* -------------------------------------------------------------------------- */
+/* mallocNewCell                                                              */
+/*       Alloue la memoire pour une cellule et initialise ses champs          */
+/*                                                                            */
+/* En entrée: (void)                                                          */
+/*                                                                            */
+/* En sortie: cell (cell_t *) : Cellule allouee                               */
+/* -------------------------------------------------------------------------- */
 cell_t * mallocNewCell();
 
+
+/* -------------------------------------------------------------------------- */
+/* createTableFromFile                                                        */
+/*       Creer la table de hachage depuis le fichier ouvert passe en parametre*/
+/*                                                                            */
+/* En entrée: file (FILE *) : Fichier a lire pour creer la table              */
+/*            hashTable (cell_t **) : Table de hachage                        */
+/*                                                                            */
+/* En sortie: errorCode (char) : code d'erreur, 1 si erreur, 0 sinon          */
+/* -------------------------------------------------------------------------- */
 char createTableFromFile(FILE * file, cell_t ** hashTable);
 
+
+/* -------------------------------------------------------------------------- */
+/* addWordInTable                                                             */
+/*       Ajoute un mot dans la table de hachage, il peut etre necessaire que  */
+/*       de la memoire soit allouee si le mot a ajouter n'est pas deja present*/
+/*                                                                            */
+/* En entrée: hashTable (cell_t **) : Table de hachage                        */
+/*            word (char *) : Mot a ajouter dans la table                     */
+/*            size (int) : Taille du mot                                      */
+/*                                                                            */
+/* En sortie: errorCode (char) : code d'erreur, 1 si erreur, 0 sinon          */
+/* -------------------------------------------------------------------------- */
 char addWordInTable(cell_t * hashTable[HASH_MAX], char * word, int size);
 
+
+/* -------------------------------------------------------------------------- */
+/* deleteWordFromTable                                                        */
+/*       Supprime un mot de la table et renvoie son nombre d'apparitions      */
+/*                                                                            */
+/* En entrée: hashTable (cell_t **) : Table de hachage                        */
+/*            word (char *) : Mot a supprimer de la table                     */
+/*                                                                            */
+/* En sortie: number (int) : Nombre d'apparitions du mot dans le fichier      */
+/* -------------------------------------------------------------------------- */
 int deleteWordFromTable(cell_t * hashTable[HASH_MAX], char * word);
 
 cell_t ** findWordInChainedList(cell_t ** prev, char * word);
